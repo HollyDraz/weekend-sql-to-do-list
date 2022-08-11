e4be2ea40d4fdd845d62d2c9a3315612e9c8c050
Holly Drazenovich <hollydraze14@gmail.com>
2022-08-11 14:19:11 -0500

    made changes to the client side server - now debugging get and post

--- servers/public/scripts/client.js
+++ servers/public/scripts/client.js
@@ -4,8 +4,29 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log("client.js ready");
+    $('#addTask').on('click',  sendListToServer);
+    //sendListToServer()
     getList()
 }
+
+
+
+function sendListToServer() {
+    console.log('in send list');
+    $.ajax({
+      type: 'POST',
+      url: '/list',
+      data: {
+        task: $('#taskToDo').val()
+      }
+    }).then( function (response) {
+        console.log('response');
+        getList(); // need to create, will display the list on the DOM
+    }).catch( function (error) {
+        console.log(error);
+        alert('Something went wrong. Please try again.');
+    });
+  }
 
 /**
  * function that will call the server to show 
@@ -16,22 +37,17 @@ function getList(){
     $.ajax({
         type: 'GET',
         url: '/list'
+    }).then(function(response) {
+        console.log("get list response", response);
+        $('#viewListTable').empty();
+        for(let i =0; i < response.length; i++) {
+            let newTask = response[i];
+            $('#viewListTable').append(`
+                <tr>
+                <td>${newTask.task}</td>
+                </tr>
+            
+            `);
+        }
     })
 }
-
-function sendListToServer() {
-    console.log('in send list');
-    $.ajax({
-      type: 'POST',
-      url: '/list',
-      data: {
-        task: $('#taskToDo').val();
-      }
-    }).then( function (response) {
-        console.log('response');
-        getTask(); // need to create, will display the list on the DOM
-    }).catch( function (error) {
-        console.log(error);
-        alert('Something went wrong. Please try again.');
-    });
-  }
--- servers/routes/todo.js
+++ servers/routes/todo.js
@@ -4,8 +4,30 @@ const todoRouter = express.Router();
 const pool = require('../modules/pool.js');
 
 //get 
+todoRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "list" ORDER BY "task";';
+    pool.query(queryText).then((results) => {
+        res.send(results.rows);
+    }).catch((error) => {
+        console.log('error in list GET', error);
+        res.sendStatus(500);
+    });
+} );
 
 //post 
+todoRouter.post('/', (req, res) => {
+    const tasks = req.body;
+    const queryText = `INSERT INTO "list" ("task")
+                       VALUES($1)`
+    pool.query(queryText,[tasks.task])
+        .then((results) => {
+            console.log(results);
+            res.send(results);
+        }).catch((error)=>{
+            console.log("error in task post", error );
+            res.sendStatus(500);
+        });
+});
 
 //put 
 
